1c0f0cffa3089506a3b33d07099b5e9f9a297e54
Markus Shepherd <markus.r.shepherd@gmail.com>
2016-10-02 02:49:55 +0300

    small modifications to chunking

--- translate.py
+++ translate.py
@@ -9,24 +9,37 @@ import sys
 with open('.api_key') as file:
     API_KEY = file.read().strip()
 
-def translate(queries, target, source=None, chunk_size=0, max_retries=3):
+def chunks(iterable, size):
+    count = 0
+    for item in iterable:
+        if count == 0:
+            chunk = []
+        chunk.append(item)
+        count += 1
+        if count == size:
+            yield chunk
+            count = 0
+    if count > 0:
+        yield chunk
+
+def translate(queries, target, source=None, chunk_size=0, max_tries=3):
     url = 'https://www.googleapis.com/language/translate/v2'
 
     queries = list(queries)
 
-    if not chunk_size:
-        chunk_size = len(queries)
+    chunks_ = chunks(queries, chunk_size) if chunk_size else [queries]
 
-    for i in range(0, len(queries), chunk_size):
+    for chunk in chunks_:
         params = {
             'key': API_KEY,
-            'q': queries[i:i+chunk_size],
+            'q': chunk,
             'target': target,
         }
+
         if source:
             params['source'] = source
 
-        for retry in range(max_retries):
+        for retry in range(max_tries):
             try:
                 response = requests.get(url, params=params)
                 response.raise_for_status()
@@ -34,10 +47,10 @@ def translate(queries, target, source=None, chunk_size=0, max_retries=3):
                 translations = body['data']['translations']
                 break
             except Exception as e:
-                print(e)
-                print('retry {}'.format(retry + 1))
-        else:
-            raise Exception()
+                print(e, file=sys.stderr)
+                print('retry {}'.format(retry + 1), file=sys.stderr)
+        else: # will be executed if no break in loop
+            raise ValueError('could not retrieve translations after {} tries'.format(max_tries))
 
         for translation in translations:
             yield translation['translatedText']
@@ -50,6 +63,8 @@ def get_args():
         help='target language')
     parser.add_argument('-s', '--source',
         help='source language (if none specified will be detected)')
+    parser.add_argument('-c', '--chunk-size', type=int, default=20,
+        help='number of translated lines per request')
     parser.add_argument('-v', '--verbose', action='count', default=0,
         help='increase verbosity (specify multiple times for more)')
 
@@ -61,5 +76,5 @@ if __name__ == '__main__':
     queries = args.queries if args.queries else sys.stdin
     queries = filter(None, map(lambda s: s.strip(), queries))
 
-    for t in translate(queries, source=args.source, target=args.target, chunk_size=10):
+    for t in translate(queries, source=args.source, target=args.target, chunk_size=args.chunk_size):
         print(t)
